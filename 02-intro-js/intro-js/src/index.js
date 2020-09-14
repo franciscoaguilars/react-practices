e5822c88af503ca44530d18696783c13a2e594a6
Francisco Aguilar <fiaguilars@gmail.com>
2020-09-13 19:33:22 -0500

    Practice

---
+++ 02-intro-js/intro-js/src/index.js
@@ -0,0 +1,16 @@
+// Scope 
+let valor = 5;
+console.log( valor );
+
+if ( true ){
+    let valor = 10;
+
+    console.log(valor);
+}
+
+// Template String
+
+const nombreCompleto = "Francisco Aguilar";
+const saludo = `Hello, ${nombreCompleto}`
+
+console.log(saludo); 
