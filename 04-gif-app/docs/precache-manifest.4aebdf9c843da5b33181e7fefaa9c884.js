3b7e63ff891d9a81c73d6e3ff8346687b88b8b5f
Francisco Aguilar <fiaguilars@gmail.com>
2020-09-20 20:38:05 -0500

    Build

---
+++ 04-gif-app/docs/precache-manifest.4aebdf9c843da5b33181e7fefaa9c884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0001a20db91b91f2444a2c684472bb24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f468b474b337f788da1c",
+    "url": "/static/css/main.4a9debad.chunk.css"
+  },
+  {
+    "revision": "fc3f4adde8e6c677780d",
+    "url": "/static/js/2.d32d5f33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d32d5f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f468b474b337f788da1c",
+    "url": "/static/js/main.75dad192.chunk.js"
+  },
+  {
+    "revision": "9a5b112487ec48d33754",
+    "url": "/static/js/runtime-main.49d3f13c.js"
+  }
+]);
