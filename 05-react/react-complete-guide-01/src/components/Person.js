63e86aedf590057f7e7d8fcd4eaead37ad96f26e
Francisco Aguilar <fiaguilars@gmail.com>
2020-09-26 23:31:02 -0500

    Finished first part of course

--- 05-react/react-complete-guide-01/src/components/Person.js
+++ 05-react/react-complete-guide-01/src/components/Person.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './Person.css'
 const Person = (props) => {
 
     
     return(
-        <>
+        <div className= "Person">
             <h1>Hello {props.name}, your age is {props.age}</h1>
             <input type="text" onChange={props.change}/> 
-        </>
+        </div>
     )
 }
 
